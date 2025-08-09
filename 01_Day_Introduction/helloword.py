c1dd6af149d1d24b5a9ebf721acbf41de15e8c84
E.O.R_BAH545 <elle.oumarrafiou@gmail.com>
2025-08-09 11:53:43 +0000

    Create helloword.py

---
+++ 01_Day_Introduction/helloword.py
@@ -0,0 +1,22 @@
+# Introduction
+# Jour 1 - Défi 30DaysOfPython
+
+print("Hello, World!")  # Affiche un message
+print(2 + 3)  # Addition
+print(3 - 1)  # Soustraction
+print(2 * 3)  # Multiplication
+print(3 / 2)  # Division
+print(3 ** 2)  # Exponentielle
+print(3 % 2)  # Modulo
+print(3 // 2)  # Division entière
+
+# Vérification des types de données
+
+print(type(10))          # Entier
+print(type(3.14))        # Flottant
+print(type(1 + 3j))      # Complexe
+print(type("Eor_bah545"))    # Chaîne
+print(type([1, 2, 3]))   # Liste
+print(type({"nom": "Elhadj oumar rafiou bah"}))  # Dictionnaire
+print(type({9.8, 3.14}))      # Ensemble
+print(type((9.8, 3.14)))      # Tuple
